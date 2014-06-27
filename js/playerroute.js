69a1e46a9605bbd5a829ff7ebc075cd448526e3b
nitish1402 <nitish2011204@outlook.com>
2014-06-27 23:10:35 +0530

    initial commit

---
+++ js/playerroute.js
@@ -0,0 +1,26 @@
+(function(){
+
+	var playerRoute = angular.module('playerRoute',['ngRoute','playersApp']);
+
+	playerRoute.config(['$routeProvider',function($routeProvider){
+		$routeProvider.
+		when('/players',{
+			templateUrl : 'playerview.html',
+			controller : 'MyController',
+			controllerAs : 'viewP'
+		}).
+		when('/players/:playerShort_name',{
+			templateUrl : 'playerdetail.html',
+			controller : 'playerDetail'
+
+		}).
+		otherwise({
+          redirectTo: '/players'
+        });
+
+	}]);
+
+
+
+
+})();
