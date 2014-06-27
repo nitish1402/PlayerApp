4d4556d0006df395fcb47a6d87960b2615332d2e
nitish1402 <nitish2011204@outlook.com>
2014-06-28 00:29:07 +0530

    Adding pages

---
+++ js/players.js
@@ -0,0 +1,38 @@
+(function(){
+
+	var app = angular.module('playersApp',[]);
+
+	app.controller('MyController',['$http',function($http){
+		var store = this;
+		this.players = [];
+		
+		var promise = $http({
+			method : 'GET',
+			url : 'js/players.json'
+		});
+
+		promise.success(function(data){
+			store.players = data;
+
+		});
+		
+	}]
+	);
+
+	app.controller('playerDetail',['$scope','$routeParams','$http',function($scope,$routeParams,$http){
+		$scope.name = $routeParams.playerShort_name;
+		$http.get('js/'+$scope.name+'.json').success(function(data){
+			$scope.player = data;
+		});
+		//alert($scope.playerLong_name);
+	}]);
+
+	app.controller('ImageController',function($scope){
+		$scope.current = 0;
+		$scope.setCurrent = function(index)
+		{
+			$scope.current = index;
+		}
+	})
+
+})();
