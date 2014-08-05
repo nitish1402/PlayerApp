2f3a4bd1429cb3da0542066590446925d15ed1df
nitish1402 <nitish2011204@outlook.com>
2014-08-05 22:18:53 +0530

    changed directory structure

--- js/players.js
+++ js/players.js
@@ -5,11 +5,7 @@
 	app.controller('MyController',['$http',function($http){
 		var store = this;
 		this.players = [];
-<<<<<<< HEAD
-		
-=======
 
->>>>>>> master
 		var promise = $http({
 			method : 'GET',
 			url : 'js/players.json'
@@ -19,21 +15,17 @@
 			store.players = data;
 
 		});
-<<<<<<< HEAD
-		
-=======
 
->>>>>>> master
 	}]
 	);
 
 	app.controller('playerDetail',['$scope','$routeParams','$http',function($scope,$routeParams,$http){
 		$scope.name = $routeParams.playerShort_name;
-<<<<<<< HEAD
+
 		$http.get('js/'+$scope.name+'.json').success(function(data){
-=======
+
 		$http.get('js/players/'+$scope.name+'.json').success(function(data){
->>>>>>> master
+
 			$scope.player = data;
 		});
 		//alert($scope.playerLong_name);
@@ -47,8 +39,4 @@
 		}
 	})
 
-<<<<<<< HEAD
 })();
-=======
-})();
->>>>>>> master
